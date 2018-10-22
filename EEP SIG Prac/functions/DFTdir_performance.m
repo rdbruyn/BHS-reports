9ebcc5e65eff94a11711efcba733270324c3fb60
rdbruyn <bruynrd@gmail.com>
2018-10-22 07:43:44 +0200

    Did question 2.2 and 2.3 for the signals prac

---
+++ EEP SIG Prac/functions/DFTdir_performance.m
@@ -0,0 +1,11 @@
+function [avg_time, data_arr] = DFTdir_performance(input)
+  total = 0;
+  k = 1;
+  data_arr = zeros(1, k);
+  for i = 1:k
+    t = DFTdir_time(input);
+    total += t;
+    data_arr(i) = t;
+  endfor
+  avg_time = total / k;
+endfunction
