87e87ed431066e85cfc1ca679b3784c052edb336
QuintinKruger <krugerquintin5@gmail.com>
2019-02-24 16:49:19 +0200

    Discussion  addition to prac 1

---
+++ BHS4A_Reports/Practical 1/Functions/PID.m
@@ -0,0 +1,33 @@
+## Copyright (C) 2019 Quintin
+## 
+## This program is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*- 
+## @deftypefn {} {@var{retval} =} PID (@var{input1}, @var{input2})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: Quintin <quintin@quintin-Inspiron-15-3567>
+## Created: 2019-02-12
+
+function PID (Kp, Ki, Kd)
+  H = tf([-5*9.8],[7 0 0]);
+  D = pid(Kp,Ki,Kd);
+  cascade = H * D ;
+  theta = cascade/(1+cascade);
+  step(theta);
+  figure;
+  rlocus(theta);
+  endfunction
