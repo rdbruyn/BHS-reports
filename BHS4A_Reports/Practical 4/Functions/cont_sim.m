2a2ee9398d0e0dfece19dd9dcd074b0c4cca997f
QuintinKruger <krugerquintin5@gmail.com>
2019-05-20 13:24:54 +0200

    Lekker

--- BHS4A_Reports/Practical 4/Functions/cont_sim.m
+++ BHS4A_Reports/Practical 4/Functions/cont_sim.m
@@ -1,42 +1,38 @@
-## Copyright (C) 2019 root
-## 
-## This program is free software; you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 3 of the License, or
-## (at your option) any later version.
-## 
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-## 
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-## -*- texinfo -*- 
-## @deftypefn {} {@var{retval} =} cont_sim (@var{input1}, @var{input2})
-##
-## @seealso{}
-## @end deftypefn
-
-## Author: root <root@quintin-Inspiron-15-3567>
-## Created: 2019-05-16
-
-function [retval] = cont_sim (input1, input2)
+function [retval] = cont_sim (tsam,tsam1,tsam2,tsam3,tsam4)
   pkg load control;
   pkg load signal;
- 
-  plant = tf([-5*9.8],[7 0 0]);
-  cont_cont = pid(70,18,40)
-  plot(cont_cont);
-  hold on;
-  dis_cont = c2d(cont_cont,6.47,'tustin')
-  
-  step(dis_cont)
-  
-  
-  
 
+  plant = tf([5*9.8],[7 0 0])
+  plant_dis_tsam1 = c2d(plant,tsam1,'tustin')
+  plant_dis_tsam2 = c2d(plant,tsam2,'tustin')
+  plant_dis_tsam3 = c2d(palnt,tsam3,'tustin')
+  plant_dis_tsam4 = c2d(pant,tsam4,'tustin')
   
+  cont_cont = 0.28*tf([6.7 1],[0.106*6.7 1])
 
+  feedback_cont = feedback(plant*cont_cont);
+  #step(feedback_cont)
+  # Rise time of 0.26882
+  
+  dis_cont_tus_tsam1 = c2d(cont_cont,tsam1,'tustin');
+  dis_cont_tus_tsam2 = c2d(cont_cont,tsam2,'tustin');
+  dis_cont_tus_tsam3 = c2d(cont_cont,tsam3,'tustin');
+  dis_cont_tus_tsam4 = c2d(cont_cont,tsam4,'tustin');
+  
+  
+  feedback_dis_tus_tsam1 = feedback(plant_sampled*dis_cont_tus_tsam1);
+  feedback_dis_tus_tsam2 = feedback(plant_sampled*dis_cont_tus_tsam2);
+  feedback_dis_tus_tsam3 = feedback(plant_sampled*dis_cont_tus_tsam3);
+  feedback_dis_tus_tsam4 = feedback(plant_sampled*dis_cont_tus_tsam4);
+  
+  subplot(4,1,1);
+  step(feedback_dis_tus_tsam1)
+  subplot(4,1,2);
+  step(feedback_dis_tus_tsam2)
+  subplot(4,1,3);
+  step(feedback_dis_tus_tsam3)
+  subplot(4,1,4);
+  step(feedback_dis_tus_tsam4)
+  
+  
 endfunction
