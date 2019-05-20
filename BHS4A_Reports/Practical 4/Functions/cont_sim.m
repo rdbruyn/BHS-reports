bde7c4fedb571a22a1c8364728973d3c38027fb9
QuintinKruger <krugerquintin5@gmail.com>
2019-05-20 13:26:34 +0200

    Lekker

--- BHS4A_Reports/Practical 4/Functions/cont_sim.m
+++ BHS4A_Reports/Practical 4/Functions/cont_sim.m
@@ -5,8 +5,8 @@ function [retval] = cont_sim (tsam,tsam1,tsam2,tsam3,tsam4)
   plant = tf([5*9.8],[7 0 0])
   plant_dis_tsam1 = c2d(plant,tsam1,'tustin')
   plant_dis_tsam2 = c2d(plant,tsam2,'tustin')
-  plant_dis_tsam3 = c2d(palnt,tsam3,'tustin')
-  plant_dis_tsam4 = c2d(pant,tsam4,'tustin')
+  plant_dis_tsam3 = c2d(plant,tsam3,'tustin')
+  plant_dis_tsam4 = c2d(plant,tsam4,'tustin')
   
   cont_cont = 0.28*tf([6.7 1],[0.106*6.7 1])
 
@@ -20,10 +20,10 @@ function [retval] = cont_sim (tsam,tsam1,tsam2,tsam3,tsam4)
   dis_cont_tus_tsam4 = c2d(cont_cont,tsam4,'tustin');
   
   
-  feedback_dis_tus_tsam1 = feedback(plant_sampled*dis_cont_tus_tsam1);
-  feedback_dis_tus_tsam2 = feedback(plant_sampled*dis_cont_tus_tsam2);
-  feedback_dis_tus_tsam3 = feedback(plant_sampled*dis_cont_tus_tsam3);
-  feedback_dis_tus_tsam4 = feedback(plant_sampled*dis_cont_tus_tsam4);
+  feedback_dis_tus_tsam1 = feedback(plant_dis_tsam1*dis_cont_tus_tsam1);
+  feedback_dis_tus_tsam2 = feedback(plant_dis_tsam2*dis_cont_tus_tsam2);
+  feedback_dis_tus_tsam3 = feedback(plant_dis_tsam3*dis_cont_tus_tsam3);
+  feedback_dis_tus_tsam4 = feedback(plant_dis_tsam4*dis_cont_tus_tsam4);
   
   subplot(4,1,1);
   step(feedback_dis_tus_tsam1)
